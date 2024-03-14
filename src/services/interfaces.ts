694c1b91d1ce4682ddb8c02bbb2f7a6378f65967
Adelkilde <William.adelkilde@gmail.com>
2024-03-14 13:52:44 +0100

    interfaces + api created

---
+++ src/services/interfaces.ts
@@ -0,0 +1,46 @@
+interface Booking {
+  id: string;
+  customer: Customer[];
+  show: Show[];
+  seats: Seat[];
+}
+interface Category {
+  id: number | null;
+  name: string;
+}
+interface Movie {
+  id: number | null;
+  title: string;
+}
+interface Seat {
+  id: number | null;
+  seatRow: number;
+  seatNr: number;
+  available: boolean;
+  theater: Theater[];
+  type: string;
+}
+interface Show {
+  id: number | null;
+  startTime: Date;
+  endTime: Date;
+  movie: Movie[];
+  theater: Theater[];
+}
+interface Theater {
+  id: number;
+  name: string;
+}
+interface Customer {
+  firstName: string;
+  middleName?: string;
+  lastName: string;
+  email: string;
+  streetAddress: string;
+  streetNo: string;
+  zip: string;
+  country: string;
+  phone: string;
+}
+
+export type { Booking, Category, Movie, Seat, Show, Theater, Customer };
