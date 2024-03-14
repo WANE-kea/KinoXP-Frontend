2a7658fe991f63f65e56bf2c826cc3fd9f3097ca
Nikolaj Christian Møller <nikolajchristianmoeller@gmail.com>
2024-03-14 13:33:39 +0100

    AuthStatus.tsx
    Lavede AuthStatus.tsx færdigt

---
+++ src/security/AuthStatus.tsx
@@ -0,0 +1,20 @@
+import { Link, NavLink } from "react-router-dom";
+import { useAuth } from "./AuthProvider";
+
+export default function AuthStatus() {
+    const auth = useAuth();
+
+    if (!auth.isLoggedIn()) {
+        return (
+            <li>
+                <NavLink to="/login">Login</NavLink>
+            </li>
+        );
+    } else {
+        return (
+            <li>
+                <Link to="/logout">Logout (Logged in as {auth.username})</Link>
+            </li>
+        );
+    }
+}
