2ff0d05865e0b7970f3f69494473b32201aa2e40
Nikolaj Christian Møller <nikolajchristianmoeller@gmail.com>
2024-03-14 14:20:53 +0100

    Tommer linjer slet
    Indrykkede bare linjer ind

--- src/security/RequireAuth.tsx
+++ src/security/RequireAuth.tsx
@@ -1,15 +1,13 @@
 import { useAuth } from "./AuthProvider";
 import { Navigate, useLocation } from "react-router-dom";
 
-
 type Props = {
     children: JSX.Element;
     roles?: string[];
 };
+
 export default function RequireAuth({ children, roles }: Props) {
     const auth = useAuth();
-
-
     const location = useLocation();
     if (roles) {
         if (!auth.isLoggedInAs(roles)) {
