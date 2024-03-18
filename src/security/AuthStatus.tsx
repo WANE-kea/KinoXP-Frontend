849230d49c444877c05bc24ed2785f641a40c158
Adelkilde <William.adelkilde@gmail.com>
2024-03-18 12:16:20 +0100

    App import + prettier formatting

--- src/security/AuthStatus.tsx
+++ src/security/AuthStatus.tsx
@@ -2,19 +2,19 @@ import { Link, NavLink } from "react-router-dom";
 import { useAuth } from "./AuthProvider";
 
 export default function AuthStatus() {
-    const auth = useAuth();
+  const auth = useAuth();
 
-    if (!auth.isLoggedIn()) {
-        return (
-            <li>
-                <NavLink to="/login">Login</NavLink>
-            </li>
-        );
-    } else {
-        return (
-            <li>
-                <Link to="/logout">Logout (Logged in as {auth.username})</Link>
-            </li>
-        );
-    }
+  if (!auth.isLoggedIn()) {
+    return (
+      <li>
+        <NavLink to="/login">Login</NavLink>
+      </li>
+    );
+  } else {
+    return (
+      <li>
+        <Link to="/logout">Logout (Logged in as {auth.username})</Link>
+      </li>
+    );
+  }
 }
