db162834f77aadb4d852ec00bffd65827a3412e6
ReubenSkimmel <123992392+JaDetGodtDu@users.noreply.github.com>
2024-03-20 12:36:03 +0100

    Logout working !

--- src/security/AuthStatus.tsx
+++ src/security/AuthStatus.tsx
@@ -1,29 +1,23 @@
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { useAuth } from "./AuthProvider";
-import Logout from "./Logout";
 import styles from "./AuthStatus.module.css"; // Assume you have this CSS module
 
 export default function AuthStatus() {
   const auth = useAuth();
-  console.log(auth);
   console.log("auth.isLoggedIn() = ", auth.isLoggedIn());
   if (!auth.isLoggedIn()) {
-    console.log("if er true");
-
     return (
       <NavLink to="/login" className={styles.loginLink}>
         Login
       </NavLink>
     );
   } else {
-    console.log("if er false");
     return (
       <div className={styles.authStatus}>
         <span className={styles.username}>{auth.username}</span>
         <div onClick={auth.signOut} className={styles.logoutLink}>
           Logout
         </div>
-        {/* <Logout /> */}
       </div>
     );
   }
